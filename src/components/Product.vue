a763a7dc4fcf5c164338b6a67ed14d5f8969069b
Nikandros1997 <m.nik1997@hotmail.com>
2020-01-15 13:07:23 +0000

    new features added

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,14 +1,63 @@
 <template>
-  <div>Edit me!</div>
+  <div
+    class="product"
+    :style="{
+      backgroundImage: 'url(' + product.image + ')',
+      display: 'flex',
+      alignItems: 'flex-end',
+      overflow: 'hidden'
+    }"
+  >
+    <div
+      :style="{
+        color: 'white',
+        backgroundColor: 'rgba(0,0,0,0.7)',
+        width: '100%',
+        textAlign: 'left',
+        padding: '5px'
+      }"
+    >
+      <h4>{{ product.title }}</h4>
+      <div :style="{ display: 'flex' }">
+        <h5 v-bind:class="{ oldPrice: product.discount !== null }">
+          {{ product.price }}
+        </h5>
+        <h5 :style="{ marginLeft: '15px' }">
+          {{
+            product.discount !== null
+              ? parseInt(product.price) -
+                (parseInt(product.price) *
+                  parseInt(
+                    product.discount.substring(0, product.discount.length - 1)
+                  )) /
+                  100.0
+              : ""
+          }}
+        </h5>
+      </div>
+      <p :style="{ fontSize: '12px' }">{{ product.description }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Product"
+  name: "Product",
+  props: ["product"]
 };
 </script>
 
 <style lang="scss">
 .product {
+  height: 15rem;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  border-radius: 8px;
+  vertical-align: bottom;
+  padding: 0;
+}
+.oldPrice {
+  text-decoration: line-through;
 }
 </style>
--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -1,38 +1,106 @@
 <template>
-  <div>
+  <b-container>
     <label for="selector">
       Filter:
-      <select v-model="select" id="selector">
+      <select v-model="selectedFilter" id="selector">
         <option value="all" selected>All</option>
-        <option value="test" selected>test</option>
+        <option value="purchased" selected>Purchased</option>
+        <option value="unpurchased" selected>Unpurchased</option>
+        <option value="onetime" selected>One time</option>
+        <option value="subscriptions" selected>Subscriptions</option>
       </select>
     </label>
     <h1>SELECTED FILTER: {{ selectedFilter }}</h1>
-    <div v-for="(product, idx) in products" :key="idx">
-      {{ product ? product.title : "" }}
-    </div>
-  </div>
+    <b-row :style="{ maxWidth: '1600px' }">
+      <b-col
+        sm="12"
+        md="6"
+        lg="4"
+        xl="3"
+        v-for="(product, idx) in products"
+        :key="idx"
+      >
+        <Product v-bind:product="product" />
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 const productItems = require("@/assets/products.json");
 
+import Product from "../components/Product.vue";
+
 export default {
   name: "ProductList",
+  components: {
+    Product
+  },
   computed: {
     products() {
-      if (this.selectedFilter == "all") {
-        let products = [...new Array(productItems.length)];
-        for (let i = 1; i < productItems.length - 1; i -= -1) {
-          products.forEach((product, idx) => {
-            if (idx == i) {
-              products.push(productItems[idx]);
+      switch (this.selectedFilter) {
+        case "all": {
+          let products = [];
+          let min = 0;
+
+          for (let i = 0; i < productItems.length - 1; i++) {
+            min = i;
+            for (let j = i + 1; j < productItems.length; j++) {
+              if (productItems[j].order < productItems[min].order) {
+                min = j;
+              }
+            }
+
+            let temp = productItems[min];
+            productItems[min] = productItems[i];
+            productItems[i] = temp;
+          }
+
+          productItems.forEach(product => {
+            products.push(product);
+          });
+
+          return products;
+        }
+        case "subscriptions": {
+          let products = [];
+          productItems.forEach(product => {
+            if (product.type == "recurring") {
+              products.push(product);
             }
           });
+          return products;
         }
-        return products;
+        case "onetime": {
+          let products = [];
+          productItems.forEach(product => {
+            if (product.type == "onetime") {
+              products.push(product);
+            }
+          });
+          return products;
+        }
+        case "purchased": {
+          let products = [];
+          productItems.forEach(product => {
+            if (product.purchased) {
+              products.push(product);
+            }
+          });
+          return products;
+        }
+        case "unpurchased": {
+          let products = [];
+          productItems.forEach(product => {
+            if (!product.purchased) {
+              products.push(product);
+            }
+          });
+          return products;
+        }
+        default:
+          return "Product";
       }
-      return "Product";
     }
   },
   data() {
