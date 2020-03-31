988ce9c20753107664cf45695b7d8dcb83fd865f
shannabarnard <shannabarnard@gmail.com>
2020-03-31 09:32:53 +0100

    Create product component and filter by product type

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,14 +1,81 @@
 <template>
-  <div>Edit me!</div>
+  <b-card
+    class="product-card mb-3"
+    overlay
+    :style="{
+      backgroundImage: 'url(' + product.image + ')'
+    }"
+  >
+    <b-card-text class="product-card__details p-3">
+      <h5 class="mb-0">{{ product.title }}</h5>
+      <div class="price">
+        <span :class="{ original: product.discount !== null }">
+          {{ product.price }}
+        </span>
+        <span class="discounted" v-if="product.discount">
+          <!-- discounted price to go here -->
+        </span>
+      </div>
+      <p class="mt-3 mb-0">{{ product.description }}</p>
+    </b-card-text>
+  </b-card>
 </template>
 
 <script>
 export default {
-  name: "Product"
+  name: "Product",
+  props: {
+    product: {
+      type: Object,
+      required: true
+    }
+  }
 };
 </script>
 
 <style lang="scss">
-.product {
+$product-card--height: 300px;
+$product-card__details: #fff;
+$product-card__details--price-original: #777;
+$product-card__details--description: #eee;
+
+.product-card {
+  display: flex;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  height: $product-card--height;
+
+  .card-img-overlay {
+    top: auto;
+    padding: 0;
+  }
+
+  &__details {
+    background-color: rgba(0, 0, 0, 0.8);
+    color: $product-card__details;
+    width: 100%;
+    text-align: left;
+
+    .price {
+      font-weight: bold;
+
+      .original {
+        color: $product-card__details--price-original;
+        font-size: 0.85rem;
+        margin-right: 1rem;
+        text-decoration: line-through;
+
+        &:before {
+          content: "Was";
+        }
+      }
+    }
+
+    p {
+      font-size: 0.85rem;
+      color: $product-card__details--description;
+    }
+  }
 }
 </style>
--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -1,49 +1,79 @@
 <template>
-  <div>
-    <label for="selector">
-      Filter:
-      <select v-model="select" id="selector">
-        <option value="all" selected>All</option>
-        <option value="test" selected>test</option>
-      </select>
-    </label>
-    <h1>SELECTED FILTER: {{ selectedFilter }}</h1>
-    <div v-for="(product, idx) in products" :key="idx">
-      {{ product ? product.title : "" }}
+  <b-container>
+    <b-row>
+      <b-col class="mb-3">
+        <b-form-select v-model="select" id="selector">
+          <b-form-select-option value="all" selected>
+            All products
+          </b-form-select-option>
+          <b-form-select-option value="purchased">
+            Purchased products
+          </b-form-select-option>
+          <b-form-select-option value="unpurchased">
+            Unpurchased products
+          </b-form-select-option>
+          <b-form-select-option value="onetime">
+            One time purchases
+          </b-form-select-option>
+          <b-form-select-option value="subscriptions">
+            Subscriptions
+          </b-form-select-option>
+        </b-form-select>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col
+        sm="12"
+        md="6"
+        lg="4"
+        v-for="(product, index) in products"
+        :key="index"
+      >
+        <Product :product="product" />
+      </b-col>
+    </b-row>
+    <div class="mt-3">
+      SELECTED FILTER: <strong>{{ productFilter }}</strong>
     </div>
-  </div>
+  </b-container>
 </template>
 
 <script>
-const productItems = require("@/assets/products.json");
+const products = require("@/assets/products.json");
+import Product from "../components/Product.vue";
 
 export default {
   name: "ProductList",
+  components: {
+    Product
+  },
   computed: {
     products() {
-      if (this.selectedFilter == "all") {
-        let products = [...new Array(productItems.length)];
-        for (let i = 1; i < productItems.length - 1; i -= -1) {
-          products.forEach((product, idx) => {
-            if (idx == i) {
-              products.push(productItems[idx]);
-            }
-          });
-        }
-        return products;
+      switch (this.select) {
+        case "all":
+          return products.slice().sort((a, b) => a.order - b.order);
+        case "subscriptions":
+          return products.filter(product => product.type === "recurring");
+        case "onetime":
+          return products.filter(product => product.type === "onetime");
+        case "purchased":
+          return products.filter(product => product.purchased);
+        case "unpurchased":
+          return products.filter(product => !product.purchased);
+        default:
+          return [];
       }
-      return "Product";
     }
   },
   data() {
     return {
       select: "all",
-      selectedFilter: "all"
+      productFilter: "all"
     };
   },
   watch: {
-    select: function(oldVal, newVal) {
-      this.selectedFilter = newVal;
+    select: function(newVal) {
+      this.productFilter = newVal;
     }
   }
 };
